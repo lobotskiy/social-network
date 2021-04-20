c48b3e0f1f69ceb4458c833f02967003a9c034b9
lobotskiy <lobotskiy09@gmail.com>
2021-04-20 17:14:32 +0300

    edited file App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React from 'react'
 import styled from 'styled-components';
-import Button from './components/Button';
 import Login from './components/Login';
-import Flex from './components/Flex';
 
 const Title = styled.h1`
 color: #3498db;
@@ -11,9 +9,9 @@ color: #3498db;
 function App() {
   return (
     <>
-      <Flex justify='center'>
+      <div>
         <Title>Hello!</Title>
-      </Flex>
+      </div>
       <Login />
     </>
   )
